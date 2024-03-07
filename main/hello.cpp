04e51878500bacd3cd01682a4549b34cc9d1b0b3
Vaishr2k23 <vaishnavihup@gmail.com>
2024-03-07 23:26:27 +0530

    second changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Just a small change\n";
   cout << "I have successfully built and run\n";
+  cout << "Added webhook\n";
   return 0;
 }
